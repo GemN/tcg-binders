39026dce03df9c2d217c6f0508ec726e895f7a2e
GemN <geoffrey.merran@gmail.com>
2026-07-26 11:59:31 +0700

    Keep current page after logout

--- @app/client/src/hooks/useLogOut.ts
+++ @app/client/src/hooks/useLogOut.ts
@@ -1,6 +1,5 @@
 import { useApolloClient } from "@apollo/client";
 import { useCallback } from "react";
-import { useNavigate } from "react-router";
 
 import { handleError } from "@/lib/error.ts";
 import supabase from "@/lib/supabase.ts";
@@ -8,7 +7,6 @@ import { useUserContext } from "@/providers/UserContextContext.tsx";
 
 export function useLogOut() {
   const client = useApolloClient();
-  const navigate = useNavigate();
   const { clearContext } = useUserContext();
   return useCallback(async () => {
     try {
@@ -18,11 +16,13 @@ export function useLogOut() {
       }
       await client.resetStore();
       clearContext();
+      if (window.location.pathname === "/logout") {
+        window.location.href = "/login";
+      }
     } catch (e) {
       handleError(e);
       // Something went wrong; redirect to /logout to force logout.
       window.location.href = "/logout";
     }
-    navigate("/login");
-  }, [client, clearContext, navigate]);
+  }, [client, clearContext]);
 }
